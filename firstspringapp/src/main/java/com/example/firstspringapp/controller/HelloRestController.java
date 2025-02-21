6ccc3f490ecf092b7f87b27fb067827b7082dd9b
Neeraj9080 <neerajpatel02468@gmail.com>
2025-02-21 23:56:43 +0530

    [Neeraj] Add. HelloRestController with multiple endpoint mappings

---
+++ firstspringapp/src/main/java/com/example/firstspringapp/controller/HelloRestController.java
@@ -0,0 +1,47 @@
+package com.example.firstspringapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.example.firstspringapp.user.User;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController
+{
+    // curl localhost:8080/hello -w "\n"
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello(){
+        return "Hello From BridgeLabz!!!";
+    }
+
+    // curl localost:8080/hello/query?name=John -w "\n"
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return " Hello " + name + " ! ";
+    }
+
+    // curl localhost:8080/hello/param/John -w "\n"
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name)
+    {
+        return "Hello " + name + "!";
+    }
+
+    //curl -X POST -H "Content-Type: application/json"
+    // -d '{"firstName": John" , "lastName": "Cena"}'
+    // "http://localhost:8080/hello/post" -w "\n"
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user){
+        return "Hello " + user.getFirstName() + " "+ user.getLastName() +"!";
+    }
+
+    // curl -X PUT localhost:8080/hello/put/John/?lastName=Cena -w "\n"
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,@RequestParam(value = "lastName") String lastName){
+        return "Hello "+ firstName + " "+ lastName +"!";
+    }
+}
+
+
+
